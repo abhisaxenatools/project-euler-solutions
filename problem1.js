a65b6f0596162016c23040b3dd06a0013b61d6e9
Abhishek Saxena <abhisaxenatools@gmail.com>
2016-08-12 22:59:05 +0530

    added code

---
+++ problem1.js
@@ -0,0 +1,9 @@
+// This problem is solved in JS as per the requirement of TOP.
+
+var sum = 0;
+for (var i = 0; i < 1000; i++) {
+	if ( i % 3 === 0 || i % 5 === 0) {
+		sum = sum + 1;
+	}
+}
+console.log(sum);
